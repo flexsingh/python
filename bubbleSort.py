64a80818049565b98e0cc5b78ebb4bfffc590b58
Denis Jackman <denis_jackman@hotmail.com>
2022-06-24 12:16:35 +0100

    sorted out some minro issues

--- bubbleSort.py
+++ bubbleSort.py
@@ -3,18 +3,26 @@
 '''
 
 
-def bubbleSort(list):
+def bubbleSort(sort_list):
     '''
         bubblesort routine
     '''
-    print("original list:", list)
-    length = len(list)
+    length = len(sort_list)
     for i in range(length - 1):
         for j in range(length-1-i):
-            if (list[j] > list[j+1]):
-                list[j], list[j+1] = list[j+1], list[j]
-    print("sorted list:", list)
+            if (sort_list[j] > sort_list[j+1]):
+                sort_list[j], sort_list[j+1] = sort_list[j+1], sort_list[j]
+    return sort_list
 
 
-list1 = [100, 10, 3, 56, 4, 2, 65, 76, 23]
-bubbleSort(list1)
+def main():
+    '''
+        this is the main routine
+    '''
+    list1 = [100, 10, 3, 56, 4, 2, 65, 76, 23]
+    list2 = [2, 3, 4, 10, 23, 56, 65, 76, 100]
+    print(list1, bubbleSort(list1) == list2)
+
+
+if __name__ == '__main__':
+    main()
